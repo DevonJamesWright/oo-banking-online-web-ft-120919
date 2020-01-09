3a22589fe55bd63000a2affca0d563eecd6e5ad9
Devon James Wright <devonjamess.wright@gmail.com>
2020-01-09 19:05:59 +0000

    Automatically backed up by Learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -15,8 +15,8 @@ class Transfer
   end 
   def execute_transaction
     if @sender.valid? && @receiver.valid?
-      @sender.balance - @amount
-      @receiver.balance + @amount
+      @sender.balance -= @amount
+      @receiver.balance += @amount
       @status = 'complete'
     else 
       @status = 'rejected'
