28e011ae6f11f3d80753880afa53f4c351822ab1
Devon James Wright <devonjamess.wright@gmail.com>
2020-01-09 18:43:53 +0000

    Automatically backed up by Learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -18,7 +18,7 @@ class Transfer
       @status = 'complete'
     else 
       @status = 'rejected'
-      puts "Transaction rejected. Please check your bank account."
+      p "Transaction rejected. Please check your bank account."
     end
   end 
   #def reverse_transfer
