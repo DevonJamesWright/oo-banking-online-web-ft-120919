0acb831456ce6689f8e8cafc2803e6a88d95d051
Devon James Wright <devonjamess.wright@gmail.com>
2020-01-09 18:30:23 +0000

    Automatically backed up by Learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -1,3 +1,16 @@
 class Transfer
-
+  attr_accessor :sender, :reciever, :status, :transfer_amount 
+  def initialize(sender,receiver,transfer_amount)
+    @sender = sender
+    @receiver = receiver
+    @status = "pending"
+    @transfer_amount = transfer_amount
+  end 
+  def valid?
+    if @sender.valid? && @receiver.valid?
+      true 
+    end 
+  end 
+  def execute_transaction
 end
+
